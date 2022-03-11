a880ceb0e2f56e4ee2bc609d62dc787cc2291232
manju-abhinandana <manju.na87@gmail.com>
2022-03-11 10:32:09 -0800

    added correct app

--- app.R
+++ app.R
@@ -1,55 +1,84 @@
 library(dash)
-library(dashCoreComponents)
-library(dashHtmlComponents)
 library(dashBootstrapComponents)
+library(dashHtmlComponents)
+library(dashCoreComponents)
 library(ggplot2)
 library(plotly)
+library(readr)
+library(dplyr)
+library(stringr)
+library(lubridate)
+
+url = 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-11/stressor.csv'
+stressor <- readr::read_csv(url)
+
+stressor['start_month'] <- stringr::str_split_fixed(stressor$months, '-', 2)[,1]
+stressor['year'] <- as.character(stressor$year)
+stressor['start_month'] <- as.integer(factor(stressor$start_month, levels = month.name))
+stressor["time"] <- paste0(stressor$year,"-",stressor$start_month,"-","01")
+stressor <- stressor |> select(-c(year, months, start_month))
+stressor["period"] = paste0(year(stressor$time), "-", quarter(stressor$time))
+
+
+start_date = unique(stressor[['period']])
+end_date = unique(stressor[['period']])
+state = unique(stressor[['state']])
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
-msleep2 <- readr::read_csv(here::here('data', 'msleep.csv'))
-
 app$layout(
-    dbcContainer(
-        list(
-            htmlH1('Dashr heroky deployment'),
-            dccGraph(id='plot-area'),
-            htmlDiv(id='output-area'),
-            htmlBr(),
-            htmlDiv(id='output-area2'),
-            htmlBr(),
+  dbcContainer(
+    dbcRow(
+      list(dccGraph(id='plot-area'),
+        dbcCol(
+          list(
+            htmlLabel('State'),
             dccDropdown(
-                id='col-select',
-                options = msleep2 %>% colnames %>% purrr::map(function(col) list(label = col, value = col)),
-                value='bodywt')
-        )
-    )
+              id='state',
+              options = state,
+              value='Alabama')
+            )
+          ),
+        dbcCol( 
+          list(
+            htmlLabel('Start time'),
+            dccDropdown(
+              id='start_time',
+              options = start_date,
+              value='2015-1')
+            )
+          ),
+        dbcCol(
+          list(
+            htmlLabel('End time'),
+            dccDropdown(
+              id='end_time',
+              options = end_date,
+              value='2015-4')
+            )
+          )
+  )
 )
+)
+)
+
 
 app$callback(
-    output('plot-area', 'figure'),
-    list(input('col-select', 'value')),
-    function(xcol) {
-        p <- ggplot(msleep2) +
-            aes(x = !!sym(xcol),
-                y = sleep_total,
-                color = vore,
-                text = name) +
-            geom_point() +
-            scale_x_log10() +
-            ggthemes::scale_color_tableau()
-        ggplotly(p) %>% layout(dragmode = 'select')
-    }
+  output('plot-area', 'figure'),
+  list(input('state', 'value'),
+       input('start_time', 'value'),
+       input('end_time', 'value')),
+  function(state, start_time, end_time) {
+    p <- stressor %>%  filter(state == {{state}} & (period >= {{start_time}} & period <= {{end_time}})) %>%
+      ggplot(aes(x = period,
+                 y = stress_pct,
+                 fill = stressor,
+                 text = stress_pct)) +
+      geom_bar(position="stack", stat="identity") 
+      labs(title = 'Bee colony stressors', x = 'Time period', y = 'Impacted colonies(%)')
+  ggplotly(p,  tooltip = state)
+  }
 )
 
-app$callback(
-    list(output('output-area', 'children'),
-         output('output-area2', 'children')),
-    list(input('plot-area', 'selectedData'),
-         input('plot-area', 'hoverData')),
-    function(selected_data, hover_data) {
-        list(toString(selected_data), toString(hover_data))
-    }
-)
-
+# app$run_server(debug = T)
 app$run_server(host = '0.0.0.0')
--- init.R
+++ init.R
@@ -9,5 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes'))
+install.packages(c('dash', 'readr', 'ggplot2', 'plotly', 'dplyr', 'stringr', 'lubridate', 'dashHtmlComponents', 'dashCoreComponents'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
