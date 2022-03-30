514dd642955fb35811e72c645647ceefc88c74ca
manju-abhinandana <manju.na87@gmail.com>
2022-03-29 17:23:44 -0700

    add all widgets

--- app.R
+++ app.R
@@ -13,14 +13,12 @@ library(lubridate)
 stressor <-  readr::read_csv("data/stressor.csv")
 stressor
 
-# url = 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-11/stressor.csv'
-# stressor <- read_csv(url)
 
 # Wrangle data
 stressor <- stressor %>%
   tidyr::separate(months, into = c("start_month","start_month2"), sep = "-") %>%
   dplyr::mutate(time = lubridate::ym(paste(year, start_month)),
-         period = lubridate::quarter(time, type = "year.quarter")) %>% 
+                period = lubridate::quarter(time, type = "year.quarter")) %>% 
   dplyr::select(state, stressor,stress_pct, time, period)
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
@@ -33,8 +31,30 @@ app$layout(
         id = "state-widget",
         options = unique(stressor$state),
         value = "Alabama"
+      ),
+      dccDropdown(
+        id = "start-date-widget",
+        options = stressor$period %>%
+          unique() %>%
+          purrr::map(function(p)
+            list(
+              label = stringr::str_replace(as.character(p), stringr::fixed("."), "Q"),
+              value = p
+            )),
+        value = 2015.1
+      ),
+      dccDropdown(
+        id = "end-date-widget",
+        options = stressor$period %>%
+          unique() %>%
+          purrr::map(function(p)
+            list(
+              label = stringr::str_replace(as.character(p), stringr::fixed("."), "Q"),
+              value = p
+            )),
+        value = 2015.4
       )
-  )
+    )
   )
 )
 
@@ -43,20 +63,28 @@ app$layout(
 app$callback(
   output('stressor_chart', 'figure'),
   list(
-    input('state-widget', 'value')
-    ),
-  plot_stressor_chart <- function(state_arg) {
-    data <- stressor %>%  filter(state == state_arg)
-    plot_stressor <- data %>% ggplot(aes(x = time,
+    input('state-widget', 'value'),
+    input('start-date-widget', 'value'),
+    input('end-date-widget', 'value') 
+  ),
+  plot_stressor_chart <- function(state_arg, start_date, end_date) {
+    start_date <- lubridate::ym(start_date)
+    end_date <- lubridate::ym(end_date)
+    
+    data <- stressor %>%  filter(state == state_arg, 
+                                 lubridate::ym(period) %within% lubridate::interval(start = start_date,
+                                                                                    end = end_date))
+    
+    plot_stressor <- data %>% ggplot(aes(x = stringr::str_replace(as.character(period), stringr::fixed("."), "Q"),
                                          y = stress_pct,
                                          fill = stressor)) +
       geom_bar(position="stack", stat="identity") + 
       theme(axis.text.x = element_text(angle = 45)) +
       labs(title = 'Bee colony stressors', x = 'Time period', y = 'Impacted colonies(%)')
     
-      ggplotly(plot_stressor)
-}
+    
+    ggplotly(plot_stressor, tooltip = c("y", "fill"))
+  }
 )
-  # app$run_server(debug = T)
 
 app$run_server(host = '0.0.0.0')
